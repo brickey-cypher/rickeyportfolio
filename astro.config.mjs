f8b86efa2a813f3a71c117431b5d134add44cf1b
brickey-cypher <brimarie7797@gmail.com>
2025-06-01 16:13:08 -0500

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import netlify from "@astrojs/netlify";
+export default defineConfig({
+  adapter: netlify(),
+});
 
-// https://astro.build/config
-export default defineConfig({});
