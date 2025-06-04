f2f7e20c7940478c0ec81769477cbe9ce42e4941
brickey-cypher <brimarie7797@gmail.com>
2025-06-04 14:44:39 -0500

    hopefully final debug for svg

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,19 +1,19 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_BqMGjeSr.mjs';
+import { manifest } from './manifest_Dxc32KxN.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
 
 const _page0 = () => import('./pages/about.astro.mjs');
-const _page1 = () => import('./pages/behind-the-scenes/preview/_slug_.astro.mjs');
-const _page2 = () => import('./pages/behind-the-scenes.astro.mjs');
+const _page1 = () => import('./pages/behind-the-scenes.astro.mjs');
+const _page2 = () => import('./pages/preview/_slug_.astro.mjs');
 const _page3 = () => import('./pages/projects.astro.mjs');
 const _page4 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["src/pages/about.astro", _page0],
-    ["src/pages/behind-the-scenes/preview/[slug].astro", _page1],
-    ["src/pages/behind-the-scenes/index.astro", _page2],
+    ["src/pages/behind-the-scenes/index.astro", _page1],
+    ["src/pages/preview/[slug].astro", _page2],
     ["src/pages/Projects.astro", _page3],
     ["src/pages/index.astro", _page4]
 ]);
@@ -26,7 +26,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "2211bd4b-6820-4729-8b00-cf7d00ea061f"
+    "middlewareSecret": "7e86ad4c-022c-4e33-a53a-fd4876d77ab4"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
